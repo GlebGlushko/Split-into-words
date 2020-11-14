febed5af1e5f0bb386fd17404d86af4e7dc203ca
GlebGlushko <gleb.glushko10@gmail.com>
2020-11-14 11:10:36 +0200

    add dp solution

--- Solution.cs
+++ Solution.cs
@@ -5,72 +5,56 @@ namespace jooble
 {
     public class Solution
     {
-        private int[] bestSplit;
-        private Dictionary<int, List<int>> graph;
+        private int[] dp;
+        private int[] ancestor;
         private Trie dictionary;
         public Solution(Trie dict)
         {
             dictionary = dict;
-            graph = new Dictionary<int, List<int>>();
         }
 
-        private void dfs(int current_ind, List<int> current_split, string original)
-        {
-            if (current_ind == original.Length)
-            {
-                if (bestSplit == null || bestSplit.Length < current_split.Count)
-                {
-                    bestSplit = new int[current_split.Count];
-                    current_split.CopyTo(bestSplit);
-                }
-                return;
-            }
-
-            foreach (int next in graph[current_ind])
-            {
-                current_split.Add(next - 1);
-                dfs(next, current_split, original);
-                current_split.Remove(current_split.Last());
-            }
-
-        }
         public string[] splitIntoWords(string word)
         {
             int len = word.Length;
             word = word.ToLower();
 
-            for (int i = 0; i < len; ++i)
-                graph[i] = new List<int>();
+            dp = new int[len + 1];
+            ancestor = new int[len + 1];
+            Array.Fill(ancestor, -1);
+            dp[0] = 1;
 
             for (int i = 0; i < len; ++i)
             {
-                Node cur;
-                if (dictionary.root.children.ContainsKey(word[i]))
-                    cur = dictionary.root.children[word[i]];
-                else
-                    continue;
+                if (dp[i] == 0 || !dictionary.root.children.ContainsKey(word[i])) continue;
+                Node cur = dictionary.root.children[word[i]];
 
                 for (int l = 0; i + l < len; ++l)
                 {
                     if (cur.isLeaf)
-                        graph[i].Add(i + l + 1);
-
+                    {
+                        if (dp[i + l + 1] < dp[i] + 1)
+                        {
+                            dp[i + l + 1] = dp[i] + 1;
+                            ancestor[i + l + 1] = i;
+                        }
+                    }
                     if (i + l + 1 < len && cur.children.ContainsKey(word[i + l + 1]))
                         cur = cur.children[word[i + l + 1]];
                     else break;
                 }
 
             }
-            dfs(0, new List<int> { }, word);
 
-            if (bestSplit == null) return new string[] { word
-};
-            string[] ans = new string[bestSplit.Length];
-            int prev = 0;
-            for (int i = 0; i < bestSplit.Length; ++i)
+            if (dp[len] == 0) return new string[] { word };
+            string[] ans = new string[dp[len] - 1];
+            int rp = len;
+            int lp = ancestor[len];
+            int it = dp[len] - 2;
+            while (lp != -1)
             {
-                ans[i] = word.Substring(prev, bestSplit[i] - prev + 1);
-                prev = bestSplit[i] + 1;
+                ans[it--] = word.Substring(lp, rp - lp);
+                rp = lp;
+                lp = ancestor[lp];
             }
             return ans;
         }
