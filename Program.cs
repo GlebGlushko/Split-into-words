21451883fa4b1c5b940e2a8e397ade79a7de7583
GlebGlushko <gleb.glushko10@gmail.com>
2020-11-14 21:44:55 +0200

    add test file, and reading and writing tests to files

--- Program.cs
+++ Program.cs
@@ -6,6 +6,10 @@ namespace jooble
 {
     class Program
     {
+        static (string, string) parse(string req)
+        {
+            return (req[0].ToString() + req[1], req.Substring(3, req.Length - 3));
+        }
         static void Main(string[] args)
         {
             const string path = @"./data/dict.txt";
@@ -23,21 +27,36 @@ namespace jooble
                 }
             }
             string request;
-            while ((request = Console.ReadLine()) != null)
+            const string test_path = @"./data/de-test-words.tsv";
+            const string res_path = @"./data/de-test-words_result.tsv";
+            using (StreamReader file = new StreamReader(test_path))
+            using (StreamWriter output = new StreamWriter(res_path))
             {
-                Solution sol = new Solution(dictionary);
-                request.ToLower();
-                var res = sol.splitIntoWords(request);
-                int len = res.Length;
-                for (int i = 0; i < len; ++i)
-                {
-                    Console.Write(res[i]);
-                    if (i < len - 1) Console.Write(", ");
-                }
-                string info = " // ";
-                info += (len > 1 ? $"разбили на {len} части" : $"невозможно разбить");
-                Console.WriteLine(info);
+                output.WriteLine(file.ReadLine());
 
+                while ((request = file.ReadLine()) != null)
+                // while ((request = Console.ReadLine()) != null)
+                {
+                    var tup = parse(request);
+                    string countryCode = tup.Item1;
+                    string word = tup.Item2;
+                    Solution sol = new Solution(dictionary);
+                    request.ToLower();
+                    var res = sol.splitIntoWords(word);
+                    int len = res.Length;
+                    output.Write($"{countryCode}    ");
+                    for (int i = 0; i < len; ++i)
+                    {
+                        // Console.Write(res[i]);
+                        // if (i < len - 1) Console.Write(", ");
+                        output.Write(res[i]);
+                        if (i < len - 1) output.Write(", ");
+                    }
+                    string info = " // ";
+                    info += (len > 1 ? $"разбили на {len} части" : $"невозможно разбить");
+                    output.WriteLine(info);
+
+                }
             }
         }
     }
